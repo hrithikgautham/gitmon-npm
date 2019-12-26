5d98db27cc065738ec9ebb36a7b71b26f1674a7e
hrithik <htg2ana@gmail.com>
2019-12-26 10:37:16 +0530

    same struct added, srcfolder deletion problem

--- utils/gittify.js
+++ utils/gittify.js
@@ -7,17 +7,24 @@ const check = require("../check");
 async function gittify(
     targetFolderName = check("target folder name must be provided!"), 
     {   
-        srcFolder = check("srcFolder is missing!"),
-        numOfChars = check("numOfChars is missing!"),
-        HALG = check("HALG is missing!"),
-        ext = check("ext is missing!"),
-        deleteSrc = typeof srcFolder === 'string' && check("deleteSrc is missing!")
+        srcFolder = check("srcFolder(String) is missing!"),
+        numOfChars = check("numOfChars(Number) is missing!"),
+        HALG = check("HALG(String) is missing!"),
+        ext = check("ext(String) is missing!"),
+        deleteSrc = typeof srcFolder === 'string' && check("deleteSrc is missing!"),
+        sameStruct = check("sameStruct(Boolean) is missing!")
     }
-) {
+) 
+{
     try {
-        // console.log("srcPAth: ", srcFolder);
-        if(numOfChars < 0)
-            throw new Error('numChars cannot be negative!');
+        if(typeof srcFolder !== 'string' && !Array.isArray(srcFolder)) // to checck if it is a string or an array
+            throw new Error(`srcFolder must be of type string or array!`);
+        if(typeof ext !== 'string' || typeof HALG !== 'string')
+            throw new Error(`ext, HALG must be of type string!`);
+        if(typeof sameStruct !== 'boolean' || typeof deleteSrc !== 'boolean')
+            throw new Error(`sameStruct, deleteSrc must be of type boolean!`);
+        if(numOfChars < 0 || typeof numOfChars !== 'number')
+            throw new Error('numChars must be of type number and cannot be negative!');
         const availableHashingAlgorithms = ['sha256', 'sha512', 'sha1'];
         const availableExtensions = ['.json', '.txt', ""];
         const mp = new Map([
@@ -30,33 +37,66 @@ async function gittify(
         if(numOfChars >= mp[HALG])
             throw new Error(`number of characters for forder name cannot exceed ${mp[HALG]} for ${HALG} hash algorithm!`);
         if(!availableExtensions.includes(ext))
-            throw new Error(`${ext} extension not available`);
-        if(typeof deleteSrc !== 'boolean')
-            throw new Error(`deleteSrc must be of type boolean!`);
-        // let srcFolder; // ..
+            throw new Error(`${ext} extension not available. Please provide extension within this array [".json", ".txt", ""]`);
+        await gittifyUtil(
+            targetFolderName, 
+            {   
+                srcFolder,
+                numOfChars,
+                HALG,
+                ext,
+                deleteSrc,
+                sameStruct
+            }
+        );
+    }
+    catch(err) {
+        throw err;
+    }
+}
+
+async function gittifyUtil(
+    targetFolderName, 
+    {   
+        srcFolder,
+        numOfChars,
+        HALG,
+        ext,
+        deleteSrc,
+        sameStruct
+    }
+) 
+{
+    try {
         let fileNames = srcFolder;
         const isSrcFolderString = typeof srcFolder === 'string';
         if(isSrcFolderString)
             fileNames = await fsPromises.readdir(srcFolder);
         for(let i = 0 ; i < fileNames.length ; i++) {
-            // console.log("stats of this file: ", stats.isDirectory(fileNames[i]));
             let originalFileOrFolder;
             let data = fileNames[i];
-            // let stat;
             if(isSrcFolderString) { // if srcPath is a string, readFile
                 originalFileOrFolder = path.join(srcFolder, fileNames[i]);
                 const stat = await fsPromises.stat(originalFileOrFolder);
                 if(stat.isDirectory()){
-                    await gittify(
+                    const temp = targetFolderName;
+                    if(sameStruct) {
+                        const folderHash = await getHash(data, "", "", HALG); // use the folder name to create the hash for name of the target folder
+                        targetFolderName = path.join(targetFolderName, folderHash);
+                        await fsPromises.mkdir(targetFolderName);
+                    }
+                    await gittifyUtil(
                         targetFolderName,
                         {
                             srcFolder: originalFileOrFolder,
                             numOfChars,
                             HALG,
                             ext,
-                            deleteSrc
+                            deleteSrc,
+                            sameStruct
                         }
                     );
+                    targetFolderName = temp;
                     continue;
                 }
                 data = await fsPromises.readFile(originalFileOrFolder, 'utf8');
@@ -64,7 +104,6 @@ async function gittify(
             data = JSON.stringify(data);
             const hash = await getHash(data, "", "", HALG);
             const folderizeFiles = await fsPromises.readdir(targetFolderName);
-            // for(let i = 0 ; i < folderizeFiles ; i++) {
             const dir = hash.slice(0, numOfChars);
             if(!folderizeFiles.includes(dir) && dir !== '')
                 await fsPromises.mkdir(path.join(targetFolderName, dir));
@@ -78,11 +117,10 @@ async function gittify(
                             targetFileNameWithExt
                         ),
                         data
-                    );
-            // uncompressed
+                    ); // uncompressed
         }
         if(isSrcFolderString) {
-            if(deleteSrc === true){
+            if(deleteSrc){
                 for(let i = 0 ; i < fileNames.length ; i++)
                     await fsPromises.unlink(path.join(srcFolder, fileNames[i]));
                 await fsPromises.rmdir(srcFolder);
@@ -90,7 +128,7 @@ async function gittify(
         }
     }
     catch(err) {
-        throw new Error('gittify failed');
+        throw err;
     }
 }
 
